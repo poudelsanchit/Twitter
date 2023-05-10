29d353c9f2761b2362741bef28ab6d74e27d33d4
Sanchit Poudel <snchtpdl12@gmail.com>
2023-05-10 21:16:35 +0545

    Learnt React Icons

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import './menu.css'
+import { FaTwitter } from "react-icons/fa";
 const Menu = () => {
     return (
         <>
             <div className="menu">
 
                 <div className="flex">
-                    <img src='images/twitter.png' className='menu-icons' />
-                    <img src='images/home.png' className='menu-icons' />
-                    <img src='images/search.png' className='menu-icons' />
-                    <img src='images/bell.png' className='menu-icons' />
-                    <img src='images/message.png' className='menu-icons' />
-                    <img src='images/bookmark.png' className='menu-icons' />
-                    <img src='images/user.png' className='menu-icons' />
-                    <img src='images/more.png' className='menu-icons' />
-                    <img src='images/message.png' className='menu-icons' />
+                    <FaTwitter className='menu-icons' />
                 </div>
             </div>
 
